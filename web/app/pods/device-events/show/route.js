e9dcd5886f2be1ecbaf0ee9b4eb16309dda2e5f2
Juan Alvarez <alce@mac.com>
2015-01-24 14:59:57 -0600

    show events

--- web/app/pods/device-events/show/route.js
+++ web/app/pods/device-events/show/route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
-    return this.soter.find('deviceEvent')
+  model: function() {
+    return this.soter.find('deviceEvent');
   }
 });
