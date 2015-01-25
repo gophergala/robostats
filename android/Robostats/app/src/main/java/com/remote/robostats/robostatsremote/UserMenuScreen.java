0dbf9fd566d6ebf7be1f8bc7317d91acf3a70c7a
Vero <veronika@ciencias.unam.mx>
2015-01-25 08:52:02 -0600

    New activities

---
+++ android/Robostats/app/src/main/java/com/remote/robostats/robostatsremote/UserMenuScreen.java
@@ -0,0 +1,17 @@
+package com.remote.robostats.robostatsremote;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * Created by vero on 1/24/15.
+ */
+public class UserMenuScreen extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_menu_activity);
+    }
+}
