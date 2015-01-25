ee87e81d4920f0e161f23b3365b72d14a1ff1564
Juan Alvarez <alce@mac.com>
2015-01-25 14:24:45 -0600

    nuke web project

--- web/app/helpers/format-date.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function formatDate(input, options) {
-  var format = options.hash['format'] || 'llll';
-  return moment(input).format(format);
-}
-
-export default Ember.Handlebars.makeBoundHelper(formatDate);
--- web/app/pods/device-classes/new/controller.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.ObjectController.extend({
-  errorMessage: null
-});
